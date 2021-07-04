d41815b68d1aae0eb3f2fd712627f11e3d832fd6
KaushikOP <72310794+KaushikOP@users.noreply.github.com>
2021-07-04 11:48:03 +0530

    jinalWaghelaBill.html
    Changed index.php to jinalWaghelaBill.html

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
         <div class="row">
             <div class="col-md-2"></div>
             
+            
             <!-- INVOICE Division -->
             <div class="col-md-7" id="invoice">
 
