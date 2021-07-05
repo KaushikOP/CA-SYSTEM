5da6e71484ca7ea20c1b7dd9bc8ac06e29a61ce4
Kaushik Talathi <kaushiktalathi491999@gmail.com>
2021-07-05 12:42:30 +0530

    Freedb.tech DB Creation.....kaushiktalathi@gmail.com....Kaushik@45...Connect.php modified.

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
-	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpass = "";
-	$db = "dmsca";
+	$dbhost = "freedb.tech";
+	$dbuser = "freedbtech_casystem";
+	$dbpass = "casystem";
+	$db = "freedbtech_CASystem";
 		
 	$con = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("unable to connect");		 
 
