cdebcc424a192f23cc63de3e95ad24918c73be19
Kaushik Talathi <kaushiktalathi491999@gmail.com>
2021-07-14 12:28:26 +0530

    Filter Animation and inlining

--- filterReceivables.php
+++ filterReceivables.php
@@ -2,13 +2,24 @@
     require_once "connect.php";
 
     if(isset($_POST)){
-        if($_POST['clientId'] == 0){
-            $query = "select * from bill_master bm inner join clientlist cl inner join firmname fn on bm.firm_id = fn.firmNameId and bm.client_id = cl.clientId where (fn.firmNameId=".$_POST['firm3']." or fn.firmNameId=".$_POST['firm2']." or fn.firmNameId=".$_POST['firm1'].") and cl.clientId>0";    
+        
+        if($_POST['firm1']==0 && $_POST['firm2']==0 && $_POST['firm3']==0){
+            if($_POST['clientId'] == 0){
+                $query = "select * from bill_master bm inner join clientlist cl inner join firmname fn on bm.firm_id = fn.firmNameId and bm.client_id = cl.clientId where (fn.firmNameId>".$_POST['firm3']." or fn.firmNameId>".$_POST['firm2']." or fn.firmNameId>".$_POST['firm1'].") and cl.clientId>0";    
+            }
+            else{
+                $query = "select * from bill_master bm inner join clientlist cl inner join firmname fn on bm.firm_id = fn.firmNameId and bm.client_id = cl.clientId where (fn.firmNameId>".$_POST['firm3']." or fn.firmNameId>".$_POST['firm2']." or fn.firmNameId>".$_POST['firm1'].") and cl.clientId=".$_POST['clientId'];
+            }
         }
         else{
-            $query = "select * from bill_master bm inner join clientlist cl inner join firmname fn on bm.firm_id = fn.firmNameId and bm.client_id = cl.clientId where (fn.firmNameId=".$_POST['firm3']." or fn.firmNameId=".$_POST['firm2']." or fn.firmNameId=".$_POST['firm1'].") and cl.clientId=".$_POST['clientId'];
+
+            if($_POST['clientId'] == 0){
+                $query = "select * from bill_master bm inner join clientlist cl inner join firmname fn on bm.firm_id = fn.firmNameId and bm.client_id = cl.clientId where (fn.firmNameId=".$_POST['firm3']." or fn.firmNameId=".$_POST['firm2']." or fn.firmNameId=".$_POST['firm1'].") and cl.clientId>0";    
+            }
+            else{
+                $query = "select * from bill_master bm inner join clientlist cl inner join firmname fn on bm.firm_id = fn.firmNameId and bm.client_id = cl.clientId where (fn.firmNameId=".$_POST['firm3']." or fn.firmNameId=".$_POST['firm2']." or fn.firmNameId=".$_POST['firm1'].") and cl.clientId=".$_POST['clientId'];
+            }
         }
-        
         echo $query;
     }
 
