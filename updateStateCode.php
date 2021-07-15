bf834bce2b0e64a7f928db4da36984329b7f3c58
Abhishek Sawant <sawantabhi091@gmail.com>
2021-07-15 20:32:22 +0530

    GSt list page added. Navbar working in all pages.

---
+++ updateStateCode.php
@@ -0,0 +1,21 @@
+<?php
+
+    include 'connect.php';
+    
+    if(isset($_POST)){
+        $sCode  = $_POST['state_code'];
+        $sName = $_POST['state_name'];
+    }else{
+        echo "ERROR!!";
+    }
+    
+    $query = "update gst_state_code_list set state_name='".$sName."' where state_code=".$sCode;
+    $result = mysqli_query($con,$query);
+
+    if($result){
+        echo 'Data updated';
+    }else{
+        echo 'Data Updation Failure';
+    }
+
+?>
