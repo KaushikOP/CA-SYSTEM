1b34d09907c91995f9ade347cf2fe57f8c642bc3
Abhishek Sawant <sawantabhi091@gmail.com>
2021-07-18 16:43:20 +0530

    Invoice Number can be fetched dynamically.

--- JS/fill.js
+++ JS/fill.js
@@ -1,126 +1,221 @@
 var counterGST = 1,
-    counterNGST = 1;
-$(document).ready(function() {
+  counterNGST = 1,
+  firmId = 0;
+$(document).ready(function () {
+  $("#fYear").change(function () {
+    var year = $(this).val();
 
-    $("#GSTForm,#NGSTForm").hide();
-
-    $("#firm").change(function() {
-        if ($(this).val() == 3) {
-            $("#firmName").text("H. Mistry & Associates");
-            $("#GSTForm").show();
-            $("#NGSTForm").hide();
-        } else {
-            if ($(this).val() == 1) {
-                $("#firmName").text("Jinal Waghela & Associates");
-            } else {
-                $("#firmName").html("MoneySphere Solutions");
-            }
-            $("#GSTForm").hide();
-            $("#NGSTForm").show();
+    $.ajax({
+      url: "getInvoiceNo.php", // your php file
+      type: "GET", // type of the HTTP request
+      data: { 
+          "fYear": year,
+          "firmId":firmId
+        },
+      success: function (result) {
+        if (result) {
+            $("#NinvoiceNumber").val(result);
         }
+      },
     });
+  });
 
-    $("#clientName").change(function() {
-        var name = $(this).val();
-        //alert(name);
 
-        var id = $('#cityname option').filter(function() {
-            console.log(this.value);
-            return this.value == name;
-        }).data('id');
+  $("#fYearGST").change(function () {
+    var year = $(this).val();
 
-        //alert(id);
-        $.ajax({
-            url: 'fetchClientList.php', // your php file
-            type: 'POST', // type of the HTTP request
-            data: { "id": id },
-            success: function(result) {
-                if(result){
-                    obj = JSON.parse(result);
-                    $("#clientAddr").val(obj[0]);
-                    $("#clientGST").val(obj[1]);
-                }
-
-            }
-        });
+    $.ajax({
+      url: "getInvoiceNo.php", // your php file
+      type: "GET", // type of the HTTP request
+      data: { 
+          "fYear": year,
+          "firmId":firmId
+        },
+      success: function (result) {
+        if (result) {
+            $("#invoiceNumber").val(result);
+        }
+      },
     });
+  });
 
 
-    $("#NclientName").change(function() {
-        var name = $(this).val();
-        //alert(name);
 
-        var id = $('#Ncityname option').filter(function() {
-            console.log(this.value);
-            return this.value == name;
-        }).data('id');
+  $("#GSTForm,#NGSTForm").hide();
 
-        //alert(id);
-        $.ajax({
-            url: 'fetchClientList.php', // your php file
-            type: 'POST', // type of the HTTP request
-            data: { "id": id },
-            success: function(result) {
-                if(result){
-                    var obj = JSON.parse(result);
-                    $("#NclientAddr").val(obj[0]);
-                    $("#NclientGST").val(obj[1]);
+  $("#firm").change(function () {
+    if ($(this).val() == 3) {
+      $("#firmName").text("H. Mistry & Associates");
+      $("#GSTForm").show();
+      $("#NGSTForm").hide();
+    } else {
+      if ($(this).val() == 1) {
+        $("#firmName").text("Jinal Waghela & Associates");
+      } else {
+        $("#firmName").html("MoneySphere Solutions");
+      }
+      $("#GSTForm").hide();
+      $("#NGSTForm").show();
+    }
+  });
 
-                }
-            }
-        });
+  $("#clientName").change(function () {
+    var name = $(this).val();
+    //alert(name);
+
+    var id = $("#cityname option")
+      .filter(function () {
+        console.log(this.value);
+        return this.value == name;
+      })
+      .data("id");
+
+    //alert(id);
+    $.ajax({
+      url: "fetchClientList.php", // your php file
+      type: "POST", // type of the HTTP request
+      data: { id: id },
+      success: function (result) {
+        if (result) {
+          obj = JSON.parse(result);
+          $("#clientAddr").val(obj[0]);
+          $("#clientGST").val(obj[1]);
+        }
+      },
     });
+  });
 
+  $("#NclientName").change(function () {
+    var name = $(this).val();
+    //alert(name);
 
-    $("#addBtn").click(function() {
+    var id = $("#Ncityname option")
+      .filter(function () {
+        console.log(this.value);
+        return this.value == name;
+      })
+      .data("id");
 
-        var pName = $('#perticularGSTName').val();
-        var id = $('#perticulersGST option').filter(function() {
-            return this.value == pName;
-        }).data('id');
-
-        $("#selectGSTParticular").before("<div class='m-3 col-auto row' id='divPerticularNo" + counterGST + "' ><input type='text' class='col-6 form-control mr-2' name='PerticularNo" + counterGST + "' value='" + $("#perticularGSTName").val() + "'readonly><input type='hidden' name='PerticularNo" + counterGST + "' value='"+id+"'><input type='number' class='form-control col-3 mr-2' name='PerticularAmt" + counterGST + "' required><button type='button' class='btn btn-danger col-auto' id='btnPerticularNo" + counterGST + "'  onclick='removeGSTDivision(" + counterGST + ")' >Remove</button></div>");
-
-        $("#perticularGSTName").val("");
-        counterGST++;
+    //alert(id);
+    $.ajax({
+      url: "fetchClientList.php", // your php file
+      type: "POST", // type of the HTTP request
+      data: { id: id },
+      success: function (result) {
+        if (result) {
+          var obj = JSON.parse(result);
+          $("#NclientAddr").val(obj[0]);
+          $("#NclientGST").val(obj[1]);
+        }
+      },
     });
+  });
 
-    $("#addNBtn").click(function() {
+  $("#addBtn").click(function () {
+    var pName = $("#perticularGSTName").val();
+    var id = $("#perticulersGST option")
+      .filter(function () {
+        return this.value == pName;
+      })
+      .data("id");
 
-        var pName = $('#NGSTPerticularName').val();
-        var id = $('#perticulersNGST option').filter(function() {
-            return this.value == pName;
-        }).data('id');
+    $("#selectGSTParticular").before(
+      "<div class='m-3 col-auto row' id='divPerticularNo" +
+        counterGST +
+        "' ><input type='text' class='col-6 form-control mr-2' name='PerticularNo" +
+        counterGST +
+        "' value='" +
+        $("#perticularGSTName").val() +
+        "'readonly><input type='hidden' name='PerticularNo" +
+        counterGST +
+        "' value='" +
+        id +
+        "'><input type='number' class='form-control col-3 mr-2' name='PerticularAmt" +
+        counterGST +
+        "' required><button type='button' class='btn btn-danger col-auto' id='btnPerticularNo" +
+        counterGST +
+        "'  onclick='removeGSTDivision(" +
+        counterGST +
+        ")' >Remove</button></div>"
+    );
 
-        $("#selectNGSTParticular").before("<div class='m-3 col-auto row' id='divPerticularNo" + counterNGST + "' ><input type='text' class='col-6 form-control mr-2' name='PerticularNo" + counterNGST + "' value='" + $("#NGSTPerticularName").val() + "'readonly><input type='hidden' name='PerticularNo" + counterNGST + "' value='"+id+"'><input type='number' class='form-control col-3 mr-2' name='PerticularAmt" + counterNGST + "' required><button type='button' class='btn btn-danger col-auto' id='btnPerticularNo" + counterNGST + "'  onclick='removeNGSTDivision(" + counterNGST + ")' >Remove</button></div>");
-        $("#NGSTPerticularName").val("");
-        counterNGST++;
+    $("#perticularGSTName").val("");
+    counterGST++;
+  });
+
+  $("#addNBtn").click(function () {
+    var pName = $("#NGSTPerticularName").val();
+    var id = $("#perticulersNGST option")
+      .filter(function () {
+        return this.value == pName;
+      })
+      .data("id");
+
+    $("#selectNGSTParticular").before(
+      "<div class='m-3 col-auto row' id='divPerticularNo" +
+        counterNGST +
+        "' ><input type='text' class='col-6 form-control mr-2' name='PerticularNo" +
+        counterNGST +
+        "' value='" +
+        $("#NGSTPerticularName").val() +
+        "'readonly><input type='hidden' name='PerticularNo" +
+        counterNGST +
+        "' value='" +
+        id +
+        "'><input type='number' class='form-control col-3 mr-2' name='PerticularAmt" +
+        counterNGST +
+        "' required><button type='button' class='btn btn-danger col-auto' id='btnPerticularNo" +
+        counterNGST +
+        "'  onclick='removeNGSTDivision(" +
+        counterNGST +
+        ")' >Remove</button></div>"
+    );
+    $("#NGSTPerticularName").val("");
+    counterNGST++;
+  });
+
+  $("#firm").change(function () {
+    firmId = $("#firm").val();
+
+    var fYear = getFinancialYear();
+
+    $.ajax({
+      method: "GET",
+      url: "getInvoiceNo.php",
+      data: {
+        "firmId":firmId,
+        "fYear":fYear
+      },
+      success: function (result) {
+        $("#invoiceNumber").val(result);
+        $("#NinvoiceNumber").val(result);
+        // console.log(result);
+      },
     });
-
-
-    $("#firm").change(function() {
-        var firmId = $('#firm').val();
-        $.ajax({
-            method: 'GET',
-            url: "getInvoiceNo.php", 
-            data: 'firmId='+firmId,
-            success: function(result){
-                $('#invoiceNumber').val(result);
-                $('#NinvoiceNumber').val(result);
-            }
-        });
-    });
-
+  });
 });
 
 function removeGSTDivision(cnt) {
-    var divId = "#divPerticularNo" + cnt;
-    $(divId).remove();
-    counterGST--;
+  var divId = "#divPerticularNo" + cnt;
+  $(divId).remove();
+  counterGST--;
 }
 
 function removeNGSTDivision(cnt) {
-    var divId = "#divPerticularNo" + cnt;
-    $(divId).remove();
-    counterNGST--;
+  var divId = "#divPerticularNo" + cnt;
+  $(divId).remove();
+  counterNGST--;
+}
+
+function getFinancialYear() {
+  var fiscalyear = "";
+  var today = new Date();
+  if (today.getMonth() + 1 <= 3) {
+    fiscalyear =
+      today.getFullYear() - 1 + "-" + ("" + today.getFullYear()).substr(2);
+  } else {
+    fiscalyear =
+      today.getFullYear() + "-" + ("" + (today.getFullYear() + 1)).substr(2);
+  }
+  return fiscalyear;
 }
--- getInvoiceNo.php
+++ getInvoiceNo.php
@@ -1,18 +1,25 @@
 <?php
-include 'connect.php';
+    include 'connect.php';
 
-if(isset($_GET)){
-    $firmId =  $_GET['firmId'];
-    setcookie("firmId",$firmId);  //firmId for bill_master insertion
-    $query = "select invoice_no from bill_master where firm_id =".$firmId." order by invoice_no DESC limit 1";
-    $result = mysqli_query($con,$query);
+    $firmId = 0;
+    $fYear = 0;
     $lastInvoiceNo = '2021-22/0';
-    if($result->num_rows>0){
-        while($row=mysqli_fetch_assoc($result)){
-            $lastInvoiceNo = '2021-22/'.(((int)(explode('/',$row['invoice_no'])[1]))+1);
-        }
-    }
 
-    echo $lastInvoiceNo;
-}
+    if(isset($_GET)){
+        // print_r($_GET);
+        $firmId =  $_GET['firmId'];
+        $fYear = $_GET['fYear'];
+        $lastInvoiceNo = $fYear."/0";
+        setcookie("firmId",$firmId);  //firmId for bill_master insertion
+        $query = "select invoice_no from bill_master where firm_id=".$firmId." and invoice_no like '".$fYear."%' order by invoice_no DESC limit 1";
+        
+        $result = mysqli_query($con,$query);
+        if($result->num_rows>0){
+            while($row=mysqli_fetch_assoc($result)){
+                $lastInvoiceNo = $fYear."/".(((int)(explode('/',$row['invoice_no'])[1]))+1);
+            }
+        }
+        echo $lastInvoiceNo;
+
+    }
 ?>
