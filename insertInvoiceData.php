531cde6fd5a586a640f8bbe40359d66a93de6305
Abhishek Sawant <sawantabhi091@gmail.com>
2021-07-25 21:38:05 +0530

    Code for Notes for Particulars added.

--- insertInvoiceData.php
+++ insertInvoiceData.php
@@ -7,38 +7,34 @@
         $firmId = $_COOKIE['firmId'];
         $clientId = $_COOKIE['clientId'];
 
-        print_r($temp);
+        // print_r($temp);
         
         $formData  = array();
         $particulars = array();
+        $notes = array();
 
         if($firmId<3){
-            foreach ($temp as $key => $value) {
-                if($key<4){
-                    array_push($formData,$value);         
-                }else if($key>=4 && $key%2==0){
-                    $particulars[$value] = $temp[$key+1];
-                }else{
-                    continue;
-                }
-            }
 
-        }else{
-            foreach ($temp as $key => $value) {
-                if($key<6){
-                    array_push($formData,$value);         
-                }else if($key>=6 && $key%2==0){
-                    $particulars[$value] = $temp[$key+1];
-                }else{
-                    continue;
-                }
-            }
+            array_push($formData,$temp[0],$temp[1],$temp[2],$temp[3]);
 
+            for($i=4;$i<sizeof($temp);$i = $i+3){
+                $particulars[$temp[$i]] = $temp[$i+2];
+                $notes[$temp[$i]] = $temp[$i+1]; 
+            }
+        }
+        else{
+            array_push($formData,$temp[0],$temp[1],$temp[2],$temp[3],$temp[4],$temp[5]);
+
+            for($i=6;$i<sizeof($temp);$i = $i+3){
+                $particulars[$temp[$i]] = $temp[$i+2];
+                $notes[$temp[$i]] = $temp[$i+1]; 
+            }
+            
         }
         
-        print_r($particulars);
-        print_r($formData);
-        return;
+        // print_r($particulars);
+        // print_r($formData);
+        // print_r($notes);
 
         $totalAmount = array_sum(array_values($particulars));
 
@@ -49,7 +45,7 @@
             $queryBillMaster = "insert into bill_master values('".$formData[0]."',".$firmId.",'".$formData[1]."',".$clientId.",".$totalAmount.",0,0,0,0,0,0,null)";
         }
 
-         print_r($formData);
+        //  print_r($formData);
         // echo "\n";
         // print_r($particulars);
 
@@ -57,7 +53,7 @@
         // echo $clientId;
         // echo $totalAmount;
         // echo $queryBillMaster;
-        return;
+
         $result = mysqli_query($con,$queryBillMaster);
         
         if($result){
@@ -67,26 +63,31 @@
                 if($firmId==3){
                     $tax_amount = $pAmount + $pAmount*0.18;
                 }
-                $queryBillDetails = "insert into bill_details (bill_master_id,firm_id,service_id,taxable_amount,tax_amount) 
-                                    values('".$formData[0]."','".$firmId."',".$pID.",".$pAmount.",".$tax_amount.")";
+                $queryBillDetails = "insert into bill_details (bill_master_id,firm_id,service_id,taxable_amount,tax_amount,service_note) 
+                                    values('".$formData[0]."','".$firmId."',".$pID.",".$pAmount.",".$tax_amount.",'".$notes[$pID]."')";
                 $result1 = mysqli_query($con,$queryBillDetails);
-
-                if($result1){
-                    echo "<script>alert('Bill Generated Successfully');</script>";
+                if($result){
+                    $flag=1;
                 }else{
-                    echo "<script>alert('Error during Bill Details Generation');</script>";
+                    $flag=0;
+                    break;
                 }
+            }
+            if($flag){
+                echo "<script>alert('Bill Generated Successfully');</script>";
+            }else{
+                echo "<script>alert('Error during Bill Details Generation');</script>";
             }
             
         }else{
             echo "<script>alert('Error during Bill Master Generation');</script>";
         }  
         
-        /*echo "
+        echo "
             <script>
                 window.location = './fill.html';
             </script>
-        ";*/  
+        ";
 
     }
 
